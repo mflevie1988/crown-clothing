6d8cf06af7ba6d2da4f4e6283d7e9af57db4c02d
Ivan Perera <ivan@majestic.com.au>
2020-01-10 15:31:01 +1100

    broken the homepage into smaller components, finished styling the menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 
 //import components
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
 	return (
