3670655c0e23a0a1d8f173d9bdc96b291eabe060
Ivan Perera <ivan@majestic.com.au>
2020-01-21 14:59:56 +1100

    Update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 //import components
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
+
 function App() {
 	return (
 		<div className='App'>
-			<HomePage />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/hats' component={HomePage} />
+			</Switch>
 		</div>
 	);
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,15 +46,11 @@ class Directory extends React.Component {
 	}
 
 	render() {
+		//<MenuItem key={id} {...otherSectionProps} /> === <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
 		return (
 			<div className='directory-menu'>
-				{this.state.sections.map(({ title, imageUrl, id, size }) => (
-					<MenuItem
-						key={id}
-						title={title}
-						imageUrl={imageUrl}
-						size={size}
-					/>
+				{this.state.sections.map(({ id, ...otherSectionProps }) => (
+					<MenuItem key={id} {...otherSectionProps} />
 				))}
 			</div>
 		);
